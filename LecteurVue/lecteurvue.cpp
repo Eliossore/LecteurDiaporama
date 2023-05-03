d1f87a32ccf516380495b19a3f4541f92e1a5624
Maxime <maximejacob4000@gmail.com>
2023-05-03 16:20:55 +0200

    changement nom dossier, fichier, class ... en LecteurVue

---
+++ LecteurVue/lecteurvue.cpp
@@ -0,0 +1,66 @@
+#include "lecteurvue.h"
+#include "ui_lecteurvue.h"
+
+LecteurVue::LecteurVue(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QObject::connect(ui->actionQuitter, SIGNAL(triggered()), this, SLOT(quitter()));
+    QObject::connect(ui->actionChargerDiaporama, SIGNAL(triggered()), this, SLOT(charger()));
+    QObject::connect(ui->actionEnleverDiaporama, SIGNAL(triggered()), this, SLOT(enlever()));
+    QObject::connect(ui->actionVitesseDeDeplacement, SIGNAL(triggered()), this, SLOT(vitesse()));
+    QObject::connect(ui->actionAProposDe, SIGNAL(triggered()), this, SLOT(apropos()));
+    QObject::connect(ui->bSuivant, SIGNAL(clicked()), this, SLOT(suivant()));
+    QObject::connect(ui->bPrecedent, SIGNAL(clicked()), this, SLOT(precedent()));
+    QObject::connect(ui->bArreter, SIGNAL(clicked()), this, SLOT(arreter()));
+    QObject::connect(ui->bLancer, SIGNAL(clicked()), this, SLOT(lancer()));
+}
+
+LecteurVue::~LecteurVue()
+{
+    delete ui;
+}
+
+void LecteurVue::quitter()
+{
+    qDebug() << "Bouton quitter";
+}
+
+void LecteurVue::charger()
+{
+    qDebug() << "Bouton changer";
+}
+
+void LecteurVue::enlever()
+{
+    qDebug() << "Bouton enlever";
+}
+
+void LecteurVue::vitesse()
+{
+    qDebug() << "Bouton vitesse";
+}
+void LecteurVue::apropos()
+{
+    qDebug() << "Bouton apropos";
+}
+void LecteurVue::precedent()
+{
+    qDebug() << "Bouton precedent";;
+}
+void LecteurVue::lancer()
+{
+    qDebug() << "Bouton lancer";
+}
+void LecteurVue::arreter()
+{
+
+    qDebug() << "Bouton arreter";
+}
+
+void LecteurVue::suivant()
+{
+    qDebug() << "Bouton suivant";
+}
